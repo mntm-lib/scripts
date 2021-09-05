16472ed1fcf82dfbe7cf3d0fe32290749554e484
eolme <im@petrov.engineer>
2021-09-05 21:38:20 +0400

    Implement custom css modules

--- config/webpack/loaders/style/loader.js
+++ config/webpack/loaders/style/loader.js
@@ -1,7 +1,38 @@
+const path = require('path');
+const crypto = require('crypto');
+
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const safePostCssParser = require('postcss-safe-parser');
 
 const targets = require('../../../targets');
+
+const MODULE_EXT = '.module.css';
+
+const isModule = (name) => name.endsWith(MODULE_EXT);
+const isModuleIndex = (name) => name.endsWith('index.module.css');
+
+const contentfulName = (context, _, localName) => {
+  if (isModuleIndex(context.resourcePath)) {
+    return `${path.basename(context.context)}__${localName}`;
+  }
+
+  if (isModule(context.resourcePath)) {
+    return `${path.basename(context.resourcePath, MODULE_EXT)}__${localName}`;
+  }
+
+  return localName;
+};
+
+const minimalName = (context, _, localName) => {
+  if (isModule(context.resourcePath)) {
+    const name = path.relative(context.rootContext, context.resourcePath) + localName;
+    const hash = crypto.createHash('md4').update(name, 'utf8').digest('hex').slice(0, 7);
+
+    return `m${hash}`;
+  }
+
+  return localName;
+};
 
 /**
  * @param {'production'|'development'} mode
@@ -22,7 +53,12 @@ module.exports = (mode = 'development') => {
     loader: require.resolve('css-loader'),
     options: {
       importLoaders: 1,
-      sourceMap: !isEnvProduction
+      sourceMap: !isEnvProduction,
+      modules: {
+        auto: isModule,
+        exportLocalsConvention: 'asIs',
+        getLocalIdent: isEnvProduction ? minimalName : contentfulName
+      }
     }
   }, {
     loader: require.resolve('postcss-loader'),
